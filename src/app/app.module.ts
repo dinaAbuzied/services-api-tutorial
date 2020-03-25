862666495309a1ecea7334aecd0d5aa0d9ab2774
Dina Ahmed Hussien <dina.hussien@eg.ibm.com>
2020-03-25 06:06:38 +0200

    added UI elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
